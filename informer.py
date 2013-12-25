f002a597f57f1df5bcba7a2cf93c79c7cbbf7e0f
Shibumi <Chris.Rebischke@gmail.com>
2013-12-25 18:57:23 +0100

    Todo: Multichannel support

---
+++ informer.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# informer.py - A simple python script for checking joining users
+# 
+# Copyright (c) 2013 by Christian Rebischke <Chris.Rebischke@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/
+#
+#====================================================================== 
+#
+# Author: Christian Rebischke alias Shibumi
+# GitHub: https://github.com/shibumi
+# Email : Chris.Rebischke@gmail.com
+# GPG: E2C0DC2A
+#
+# TODO: Multichannel support
+#
+
+
+import xchat
+__module_name__="informer"
+__module_version__="1.0"
+__module_description__="check joining users!"
+
+####CONFIG######
+CHANNEL="#CHANNEL"
+#################
+
+
+
+class informer(object):
+	def __init__(self):
+		xchat.hook_print("Join",self.onJoin)
+	
+	def onJoin(self, word, word_eol, userdata):
+		username = word[0]
+		if word[1] == CHANNEL:
+			xchat.command("ctcp %s version" % username)
+			xchat.command("QUOTE whois %s" % username)
+		
+informer()
+		
+		
+		
