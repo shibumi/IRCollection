e48976c95befab5388468b3e0b2f6f0251ac7886
Shibumi <Chris.Rebischke@gmail.com>
2014-01-26 20:40:12 +0100

    Changed format and added set ts=2 sts=2 sw=2 et

--- hostcheck.py
+++ hostcheck.py
@@ -36,64 +36,66 @@ __module_author__="shibumi"
 import xchat
 
 class hostcheck(object):
-	
-	def __init__(self):
-		xchat.hook_command("hostcheck", self.check_process, help="/hostcheck <channel>")
-		
-		
-	def check_process(self, word, word_eol, userdata):
-		self.checkchannel = word[1]
-		self.hostlist = []
-		self.kicklist = []
-		xchat.hook_print("Join", self.onJoin)
-		xchat.hook_print("Quit", self.onQuit)
-		xchat.hook_print("Part", self.onPart)
-		xchat.hook_print("Kick", self.onKick)
-		xchat.hook_print("Part with Reason", self.onPartReason)
-		self.IRCserver = xchat.get_info("server")
-		self.cnc = xchat.find_context(channel = self.checkchannel, server= self.IRCserver)
-		userlist = self.cnc.get_list("users")
-		for i in userlist:
-			if i.host not in self.hostlist:
-				self.hostlist.append(i.host)
-			elif i.host in self.hostlist:
-				self.cnc.prnt("\0034\007\002CLONE DETECTED = %s" % i.host)
-		#print(self.hostlist) #DEBUG
-		print("Script ready")
-		
-	def onJoin(self, word, word_eol, userdata):
-		#print("DEBUG:", self.checkchannel, word[1], word[2]) #DEBUG
-		if self.checkchannel == word[1]:
-			userhost = word[2]			
-			if word[0] not in self.kicklist:
-				if userhost not in self.hostlist:
-					self.hostlist.append(userhost)
-		
-				elif userhost in self.hostlist:
-					#cnc = xchat.find_context(channel = self.checkchannel)
-					self.cnc.prnt("\0034\007\002CLONE DETECTED = %s" % userhost)
-				
-	def onQuit(self, word, word_eol, userdata):
-		if word[0] in self.kicklist:
-			self.kicklist.remove(word[0])
-		if word[2] in self.hostlist:
-			self.hostlist.remove(word[2])
-			
-	def onPart(self, word, word_eol, userdata):
-		if word[0] in self.kicklist:
-			self.kicklist.remove(word[0])
-		if word[1] in self.hostlist:
-			self.hostlist.remove(word[1])
-			
-	def onPartReason(self, word, word_eol, userdata):
-		if word[0] in self.kicklist:
-			self.kicklist.remove(word[0])
-		if word[1] in self.hostlist:
-			self.hostlist.remove(word[1])
-			
-	def onKick(self, word, word_eol, userdata):
-		self.kicklist.append(word[1])	
-			
+
+  def __init__(self):
+    xchat.hook_command("hostcheck", self.check_process, help="/hostcheck <channel>")
+
+
+  def check_process(self, word, word_eol, userdata):
+    self.checkchannel = word[1]
+    self.hostlist = []
+    self.kicklist = []
+    xchat.hook_print("Join", self.onJoin)
+    xchat.hook_print("Quit", self.onQuit)
+    xchat.hook_print("Part", self.onPart)
+    xchat.hook_print("Kick", self.onKick)
+    xchat.hook_print("Part with Reason", self.onPartReason)
+    self.IRCserver = xchat.get_info("server")
+    self.cnc = xchat.find_context(channel = self.checkchannel, server= self.IRCserver)
+    userlist = self.cnc.get_list("users")
+    for i in userlist:
+      if i.host not in self.hostlist:
+        self.hostlist.append(i.host)
+      elif i.host in self.hostlist:
+        self.cnc.prnt("\0034\007\002CLONE DETECTED = %s" % i.host)
+        #print(self.hostlist) #DEBUG
+    print("Script ready")
+
+  def onJoin(self, word, word_eol, userdata):
+    #print("DEBUG:", self.checkchannel, word[1], word[2]) #DEBUG
+    if self.checkchannel == word[1]:
+      userhost = word[2]			
+      if word[0] not in self.kicklist:
+        if userhost not in self.hostlist:
+          self.hostlist.append(userhost)
+
+        elif userhost in self.hostlist:
+          #cnc = xchat.find_context(channel = self.checkchannel)
+          self.cnc.prnt("\0034\007\002CLONE DETECTED = %s" % userhost)
+
+  def onQuit(self, word, word_eol, userdata):
+    if word[0] in self.kicklist:
+      self.kicklist.remove(word[0])
+    if word[2] in self.hostlist:
+      self.hostlist.remove(word[2])
+
+  def onPart(self, word, word_eol, userdata):
+    if word[0] in self.kicklist:
+      self.kicklist.remove(word[0])
+    if word[1] in self.hostlist:
+      self.hostlist.remove(word[1])
+
+  def onPartReason(self, word, word_eol, userdata):
+    if word[0] in self.kicklist:
+      self.kicklist.remove(word[0])
+    if word[1] in self.hostlist:
+      self.hostlist.remove(word[1])
+
+  def onKick(self, word, word_eol, userdata):
+    self.kicklist.append(word[1])	
+
 hostcheck()
-		
-		
+
+#set ts=2 sts=2 sw=2 et
+
+
